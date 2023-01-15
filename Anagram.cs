f8d205256891c7b6e33315ec4b9097d4d6740306
Ashwini <salunkheashwini1998@gmail.com>
2023-01-15 22:03:43 +0530

    This is file

---
+++ Anagram.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingTechniques
+{
+    public class Anagram
+    {
+        public static void DisplayAnagram()
+        {
+            string str1 = "heart";
+            string str2 = "eartt";
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str2.ToLower().ToCharArray();
+
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+
+            string val1 = new string(ch1);
+            string val2 = new string(ch2);
+
+
+            if (val1==val2)
+            {
+                Console.WriteLine("Anagram");
+
+            }else
+            {
+                Console.WriteLine("Not Anagram");
+            }
+
+
+
+        }
+    }
+}
