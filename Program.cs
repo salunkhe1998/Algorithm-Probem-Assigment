19647639270078e62f778d4915293b427f67f985
Ashwini <salunkheashwini1998@gmail.com>
2023-01-15 21:56:28 +0530

    method calling

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using SortingTechniques;
+
+namespace SortingTechnques
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //BubbleSort bubbleSort = new BubbleSort();
+            //bubbleSort.DisplayBubbleSort();
+
+            // PrimeNumber.DisplayPrimeNumber();
+            //InsertionSort.DisplayInsertion();
+            //Anagram.DisplayAnagram();
+            PrimeNoInGIvenRange.DisplayPrimeNo();
+
+
+
+
+
+
+        }
+    }
+}
